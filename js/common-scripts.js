8be0d0242d87f47016ec7c5f28e7d0425828e518
shazzadanik12 <anikwebtricker@gmail.com>
2023-08-07 10:25:45 +0600

    TrustPilot

--- js/common-scripts.js
+++ js/common-scripts.js
@@ -5,9 +5,9 @@
 
 
         // Phone nav click function
-        $('.hamburger').click(function () {
+        $('.hamburger, .dashboard-hamburger').click(function () {
             $("body").toggleClass("navShown");
-            $(".nav-wrap").fadeToggle()
+            $(".nav-wrap, .dashboard-nav").fadeToggle()
         });
 
         
@@ -95,6 +95,67 @@
 
             });
         }
+        
+        
+        $(".company-item-bottom").each(function(){
+            var $this = $(this);
+            $this.find(" > .review-accordion-trigger").on("click touch", function(){
+                $(".company-item-bottom").removeClass("accordion-active")
+                $(".review-accordion-content").slideUp();
+                if($this.find(".review-accordion-content:visible").length){
+                    $(".company-item-bottom").removeClass("accordion-active")
+                    $(".review-accordion-content").slideUp();
+                }
+                else{
+                    $this.addClass("accordion-active")
+                    $(".review-accordion-content").slideUp();
+                    $this.find(" > .review-accordion-content").slideDown();
+                }
+            })
+        })
+        
+        
+        if ($('.three-step-slider-wrap').length) {
+            $('.three-step-slider-wrap').slick({
+                arrows: true,
+                infinite: false,
+                autoplay: false,
+                autoplaySpeed: 1500,
+                navigation:false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                centerMode: false,
+                focusOnSelect: false,
+//                responsive: [
+//
+//                    {
+//                        breakpoint: 768,
+//                        settings: {
+//                            slidesToShow: 1,
+//                            swipe: false,
+//                            autoplay: true,
+//                        }
+//                    }
+//
+//                ]
+            });
+            $(window).on('resize', function () {
+                $('.three-step-slider-wrap').slick('resize');
+
+            });
+        }
+        
+        
+        $('.analytics-tab-trigger ul li').click(function(){
+            $('.analytics-tab-trigger ul li').removeClass('analytics-active');
+            $(this).addClass('analytics-active');
+            $('.analytics-tab-item-wrap .analytics-tab-item').hide();
+
+            var activeTab = $(this).find('a').attr('href');
+            $(activeTab).fadeIn();
+            return false;
+        });
         
 		
 	})// End ready function.
